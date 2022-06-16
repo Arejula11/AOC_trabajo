df655e71e1ec60a89d90b21d48efde33ae98c47a
pabloanguss <96823028+pabloanguss@users.noreply.github.com>
2022-06-16 19:12:20 +0200

    cometado

--- prac4/prac4.s
+++ prac4/prac4.s
@@ -1,16 +1,16 @@
     AREA prog, CODE, READONLY
-	EXPORT ordena 
+	EXPORT ordena
 
 ordena push{fp,lr}
 	mov fp,sp
-	push{r0}
+	;push{r0}
 	push{r1}
 	push{r2}
-	sub r1, r1, #1
+	sub r1, r1, #1  ;n-1
 	mov r2, #0
-	push{r0}
-	push{r1}
-	push {r2}
+	push{r0}	;@T
+	push{r1}	;n-1
+	push {r2}	;0
 	bl qksort
 	pop{r2}
 	pop{r1}
@@ -23,58 +23,58 @@ ordena push{fp,lr}
 qksort push{fp, lr}
 	mov fp, sp
 	push{r0, r1, r2, r3, r4, r5, r6, r7, r8, r9}
-	ldr r2,[fp,#8]
-	mov r3, r2
-	ldr r1,[fp,#12]
-	mov r4, r1
+	ldr r2,[fp,#8]		;r2=izq
+	mov r3, r2			;r3=i
+	ldr r1,[fp,#12]		;r1=der-1
+	mov r4, r1			;r4=j
 	add r5, r1, r2
-	mov r6,r5,lsr#1
-	ldr r0,[fp,#16]
-	ldr r7,[r0, r6,lsl #2]
-while ldr r2,[fp,#8]
-	mov r3, r2
-	ldr r1,[fp,#12]
-	mov r4, r1
+	mov r6,r5,lsr#1		;r6=(der-1-izq)/2
+	ldr r0,[fp,#16]		;r0=@T
+	ldr r7,[r0, r6,lsl #2]	;r7=T[(der-1-izq)*4/2]=x
+while ldr r2,[fp,#8]	;r2=izq
+	mov r3, r2			;r3=i
+	ldr r1,[fp,#12]		;r1=der-1
+	mov r4, r1			;r4=j
 	add r5, r1, r2
-	mov r6,r5,lsr#1
-	ldr r0,[fp,#16]
-	ldr r7,[r0, r6,lsl #2]
-bucle1	ldr r8,[r0,r3, lsl #2]
-	cmp r8, r7
+	mov r6,r5,lsr#1		;r6=(der-1-izq)/2
+	ldr r0,[fp,#16]		;r0=@T
+	ldr r7,[r0, r6,lsl #2]	;r7=T[(der-1-izq)*4/2]=x
+bucle1	ldr r8,[r0,r3, lsl #2]	;r8=T[i]
+	cmp r8, r7			;T[i]<x
 	bge fin_bucle1
 	add r3, r3, #1
 	b bucle1
 fin_bucle1
-bucle2 ldr r9,[r0, r4, lsl #2]
-	cmp r7, r9
+bucle2 ldr r9,[r0, r4, lsl #2]	;r9=T[j]
+	cmp r7, r9			;T[j]>x
 	bge fin_bucle2
 	sub r4, r4, #1
 	b bucle2
 fin_bucle2
-	cmp r3, r4
+	cmp r3, r4		;i<=j
 	bgt paso
-	ldr r6, [r0,r3, lsl #2]
-	str r9, [r0, r3, lsl #2]
-	str r6, [r0, r4, lsl #2]
-	add r3, r3, #1
-	sub r4, r4, #1
+	ldr r6, [r0,r3, lsl #2]		;r6=T[i]
+	str r9, [r0, r3, lsl #2]	;w=T[i]
+	str r6, [r0, r4, lsl #2]	;T[i]=T[j]
+	add r3, r3, #1				;i++
+	sub r4, r4, #1				;j--
 paso
-	cmp r3, r4
+	cmp r3, r4		;i<=j
 	bgt fin_while
 	b while
 fin_while 
 
-if1 cmp r2, r4
+if1 cmp r2, r4		;if(iz<j)
 	bge salto1
 	push{r0}
 	push{r4}
 	push{r2}
-	bl qksort
+	bl qksort		
 	pop{r2}
 	pop{r4}
 	pop{r0}
 salto1
-if2 cmp r3, r1
+if2 cmp r3, r1		;if(iz<j)
 	bge salto2
 	push{r0}
 	push{r1}
